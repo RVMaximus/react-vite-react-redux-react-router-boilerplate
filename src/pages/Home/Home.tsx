0de79a8de0b4f8a73e480e3e1beaed22717d8ff2
Raviraja R <ravivasanth1996@gmail.com>
2025-07-01 14:56:56 +0530

    Style changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,13 @@ import type { AppDispatch, RootState } from "../../redux/store";
 import { increment } from "../../redux/slices/counterSlice";
 
 export function Home() {
-    const count = useSelector((state: RootState) => state.counter.value)
-    const dispatch = useDispatch<AppDispatch>();
-    return (
-        <>  
-            {/* This is an example usage of module css */}
-            <h1 className={styles.header}>{count}</h1>
-            <button onClick={() => dispatch(increment())}></button>
-        </>
-    )
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+  return (
+    <>
+      {/* This is an example usage of module css */}
+      <h1 className={styles.header}>{count}</h1>
+      <button onClick={() => dispatch(increment())}>Click Me</button>
+    </>
+  );
 }
